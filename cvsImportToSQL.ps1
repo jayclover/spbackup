6d9f5fdf81bdbeacfad6347cdae9ab0485007278
Jay Wen (iSoftStone) <v-jawen@microsoft.com>
2017-08-18 22:02:04 +0800

    Update to run parameterized queries for 2 columns

--- cvsImportToSQL.ps1
+++ cvsImportToSQL.ps1
@@ -1,46 +1,71 @@
 ﻿Param([string]$SQLServer = $(throw "missing SQLServer as a first argument!"), [string]$SQLDBName = $(throw "missing SQLDBName as a first argument!"), [string]$tableName = $(throw "missing cvsFile as a first argument!"), [string]$cvsFilePath = $(throw "missing cvsFile as a first argument!"))
 
 
+function exec-query( $query,$parameters=@{},$SQLConn,$timeout=30,[switch]$help){
+ if ($help){
+ $msg = @"
+Execute a sql statement.  Parameters are allowed.
+Input parameters should be a dictionary of parameter names and values.
+Return value will usually be a list of datarows.
+"@
+ Write-Host $msg
+ return
+ }
+ $SQLcmd=new-object system.Data.SqlClient.SqlCommand($query,$SQLConn)
+ $SQLcmd.CommandTimeout=$timeout
+ foreach($p in $parameters.Keys){
+ [Void] $SQLcmd.Parameters.AddWithValue("@$p",$parameters[$p])
+ }
+ $ds=New-Object system.Data.DataSet
+ $da=New-Object system.Data.SqlClient.SqlDataAdapter($SQLcmd)
+ $da.fill($ds) | Out-Null
+ 
+ return $ds
+
+}
+
+
 #Create the SQL Connection Object
 Write-Verbose "Creating SQL Connection"
 $SQLConn = New-Object System.Data.SqlClient.SqlConnection("Data Source=$SQLServer; Initial Catalog=$SQLDBName; Integrated Security=SSPI")
 $SQLConn.Open()
-#Create the SQL Command Object, to work with the Database
-$SQLCmd = $SQLConn.CreateCommand()
 
-
-
+# Quit if the SQL connection didn't open properly.
+if ($SQLConn.State -ne [Data.ConnectionState]::Open) {
+    write-host -Fore Red "Connection to DB is not open."
+    Exit
+}
 
 $spDatas = Import-Csv $cvsFilePath
 
 foreach($spData in $spDatas) {
-	$spRequestID = $spData.'requests list Linked Id'
-	if ($spRequestID -ne "1321") {
-		$deleteQuery = "delete from $tableName where [Requests List Linked ID]=$spRequestID"
+	$Title1 = $spData.'Title'
+	
+	If ($Title1) {
+		$deleteQuery = "delete from $tableName where [Title]=@Title"
 
-		$SQLCmd.CommandText = $deleteQuery
-		Write-Host -Fore Yellow "Remove the legacy row in table"
-  
-		#Execute Query
-		$SQLCmd.ExecuteNonQuery() | Out-Null 
+		Write-Host -Fore Yellow "Remove the legacy row which title is: $Title1"
+		exec-query $deleteQuery -parameter @{'Title'= $spData.'Title'} -sqlconn $sqlconn	
+	} else {
+		Write-Host -Fore white "This row didn't have value for title"
 	}
+
 }
 
-$rowNo = $NULL
 Import-Csv $cvsFilePath | % {
+	
+	$title1 = $_.'Title'
+	write-host -Fore Green "Update the row for title: $title1"
 
-	#$insertQuery = "INSERT INTO $tableName  VALUES ('$($_.'Request Stage')','$($_.'Title')','$($_.'Created By')','$($_.'Created')','$($_.'Request Type')','$($_.'Purchase Order Total')','$($_.'Expense Owner or EXT Sponsor')','$($_.'Org')','$($_.'PO Adjust'),'$($_.'Add Resource Title')','$($_.'Resource 1 PA')','$($_.'Resource 2 PA')','$($_.'Resource 3 PA')','$($_.'Resource 4 PA')','$($_.'Resource 5 PA')','$($_.'Resource 1 RC')','$($_.'Resource 2 RC')','$($_.'Resource 3 RC')','$($_.'Resource 4 RC')','$($_.'Resource 5 RC')','$($_.'group1')','$($_.'PO Amt Original')','$($_.'PO Amt Extended')','$($_.'field1')','$($_.'Extended PO Number')','$($_.'Requests List Linked ID')','$($_.'Supplier Number')','$($_.'Milestone 1 IO')','$($_.'Milestone 2 IO')','$($_.'Milestone 3 IO')','$($_.'Milestone 4 IO')','$($_.'Milestone 5 IO')','$($_.'Milestone 6 IO')','$($_.'ExtPO Org')','$($_.'EXT Bulk Submit')','$($_.'Expedite')','$($_.'ExpediteR')','$($_.'Threshold Approved')','$($_.'MyDash - Request Process')','$($_.'MyDash - Get Org')','$($_.'MyDash - Send to CRM')','$($_.'PO NUMBER')','$($_.'Item Type')','$($_.'Path')')"
-	$rowNo++
-	Write-Host -Fore Green "Updateing row# $rowNo"
-	$values = @()
-	$SQLCmd.CommandText = "INSERT INTO $tableName  VALUES ('$($_.'Request Stage')','$($_.'Title')','$($_.'Created By')','$($_.'Created')','$($_.'Request Type')','$($_.'Purchase Order Total')','$($_.'Expense Owner or EXT Sponsor')','$($_.'Org')','$($_.'PO Adjust'),'$($_.'Add Resource Title')','$($_.'Resource 1 PA')','$($_.'Resource 2 PA')','$($_.'Resource 3 PA')','$($_.'Resource 4 PA')','$($_.'Resource 5 PA')','$($_.'Resource 1 RC')','$($_.'Resource 2 RC')','$($_.'Resource 3 RC')','$($_.'Resource 4 RC')','$($_.'Resource 5 RC')','$($_.'group1')','$($_.'PO Amt Original')','$($_.'PO Amt Extended')','$($_.'field1')','$($_.'Extended PO Number')','$($_.'Requests List Linked ID')','$($_.'Supplier Number')','$($_.'Milestone 1 IO')','$($_.'Milestone 2 IO')','$($_.'Milestone 3 IO')','$($_.'Milestone 4 IO')','$($_.'Milestone 5 IO')','$($_.'Milestone 6 IO')','$($_.'ExtPO Org')','$($_.'EXT Bulk Submit')','$($_.'Expedite')','$($_.'ExpediteR')','$($_.'Threshold Approved')','$($_.'MyDash - Request Process')','$($_.'MyDash - Get Org')','$($_.'MyDash - Send to CRM')','$($_.'PO NUMBER')','$($_.'Item Type')','$($_.'Path')')"
-	for($i = 0; $i -lt @($values).Count; $i++) {
-		$SQLCmd.Parameters[$i].Value = $values[$i] | Out-Null
-	}
-  #Execute Query
-  $SQLCmd.ExecuteNonQuery() | Out-Null 
+	$parameter=@{}
+	$parameter+=@{'request' = $($_.'Request Stage')}
+	$parameter+=@{'title' = $($_.'Title')}
+	
+	$insertQuery = "INSERT INTO $tableName ([Request Stage],[Title]) VALUES (@request,@title)"
+	exec-query $insertQuery -parameter $parameter -sqlconn $sqlconn	
+	
  }
-  
   
 #Close
 $SQLConn.Close()
+
